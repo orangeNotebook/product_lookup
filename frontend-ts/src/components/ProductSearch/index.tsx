b388bb519181e3a445f2902ffbe945d1e2b3423c
Kacper Kiermasz <6529076+orangeNotebook@users.noreply.github.com>
2020-08-27 16:15:30 +0100

    use states

---
+++ frontend-ts/src/components/ProductSearch/index.tsx
@@ -0,0 +1,41 @@
+import React, { useState, FormEvent } from "react";
+
+export default function ProductSearch() {
+  let [searchedProduct, setSearchedProduct] = useState("");
+  let [searchedProducts, setSearchedProducts] = useState<string[]>([]);
+
+  const updateProduct = (e: FormEvent<HTMLInputElement>) => {
+    console.log(e.currentTarget.value);
+    setSearchedProduct(e.currentTarget.value);
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    searchedProducts.push(searchedProduct);
+    setSearchedProducts(searchedProducts);
+    setSearchedProduct("");
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Search For Product:
+          <input
+            type="text"
+            name="Product"
+            id="Product"
+            value={searchedProduct}
+            onChange={updateProduct}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <div>
+        <h2>Searched Products</h2>
+        {searchedProducts.map((product) => (
+          <p>{product}</p>
+        ))}
+      </div>
+    </div>
+  );
+}
